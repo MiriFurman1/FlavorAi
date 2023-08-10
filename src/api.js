20ff6b62f2671dcf7f127032e3de86351e0cbb3d
Miri <mirifurman@gmail.com>
2023-08-10 11:54:42 +0300

    connection to gpt 2 (untrained)

--- src/api.js
+++ src/api.js
@@ -1,17 +1,12 @@
-async function query(data) {
-    console.log(import.meta.env.VITE_API_TOKEN) 
-	const response = await fetch(
-		"https://api-inference.huggingface.co/models/MiriFur/gpt2-recipes",
-		{
-			headers: { Authorization: `Bearer ${import.meta.env.VITE_API_TOKEN}` },
-			method: "POST",
-			body: JSON.stringify(data),
-		}
-	);
+async function query(modelURL, data) {
+	console.log(modelURL);
+	const response = await fetch(modelURL, {
+		headers: { Authorization: `Bearer ${import.meta.env.VITE_API_TOKEN}` },
+		method: "POST",
+		body: JSON.stringify(data),
+	});
 	const result = await response.json();
 	return result;
 }
-
-
 
 export default query;
--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,37 +1,69 @@
 import React, { useState } from "react";
 import query from "../api";
 import Button from "./Button";
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer, toast } from "react-toastify";
+import { InfinitySpin } from "react-loader-spinner";
 
 function MainLayout() {
   const [recipe, setRecipe] = useState(null);
   const [chosenModel, setChosenModel] = useState(null);
   const [prompt, setPrompt] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   const apiRequest = () => {
-    
-    query({ inputs: prompt +". title:", parameters: { max_length: 499 } }).then(
-      (response) => {
+    setIsLoading(true);
+    let modelURL = ""; // Initialize with an empty URL
+
+    if (chosenModel === "GPT-2 Trained") {
+      modelURL =
+        "https://api-inference.huggingface.co/models/MiriFur/gpt2-recipes";
+    } else if (chosenModel === "GPT-2") {
+      modelURL = "https://api-inference.huggingface.co/models/gpt2";
+    } else if (chosenModel === "GPT-3") {
+      modelURL = "URL for GPT-3 model"; // Replace with the actual URL
+    }
+    query(modelURL, {
+      inputs: prompt + ". title:",
+      parameters: { temperature: 2, max_length: 499 },
+      options: { wait_for_model: true },
+    }).then((response) => {
+      try {
         const recipeArray = Object.entries(response);
-        const startIndex = recipeArray[0][1]?.generated_text.indexOf("##");
-        if (startIndex !== -1) {
-          const slicedText = recipeArray[0][1].generated_text.slice(0,startIndex);
-          setRecipe(slicedText);
-          console.log(slicedText);
+        if (chosenModel === "GPT-2 Trained") {
+          const startIndex = recipeArray[0][1]?.generated_text.indexOf("##");
+          if (startIndex !== -1) {
+            // Slice the end
+            const slicedText = recipeArray[0][1].generated_text.slice(
+              0,
+              startIndex
+            );
+            const slicedFromBeginning = slicedText.slice(prompt.length + 8); // Slice the beginning
+            setRecipe(slicedFromBeginning);
+            console.log(slicedFromBeginning);
+          }
+        } else {
+          setRecipe(recipeArray[0][1]?.generated_text);
+          console.log(recipeArray[0][1]?.generated_text);
         }
+
+        setPrompt("");
+      } catch (error) {
+        console.error("An error occurred:", error);
+      } finally {
+        setIsLoading(false);
       }
-    );
+    });
   };
 
   return (
-    
     <div className="h-5/6 w-screen m-0 flex justify-center items-center flex-col">
-      
       <div className="flex flex-wrap justify-center">
         <Button content={"GPT-2"} setChosenModel={setChosenModel} />
         <Button content={"GPT-2 Trained"} setChosenModel={setChosenModel} />
         <Button content={"GPT-3"} setChosenModel={setChosenModel} />
       </div>
+
+      {chosenModel&&<p className="text-white">model used: {chosenModel}</p>}
 
       <input
         type="text"
@@ -39,14 +71,16 @@ function MainLayout() {
         onChange={(e) => setPrompt(e.target.value)}
         className="border rounded p-2 m-2 w-2/6"
         placeholder="Enter your prompt here"
-  
       />
 
-      <button className="bg-emerald-700 w-30 h-10 m-5" onClick={apiRequest}>
-        Generate
-      </button>
+      <div className="flex">
+        <button className="bg-emerald-700 w-30 h-10 m-5" onClick={apiRequest}>
+          Generate
+        </button>
 
-      <div className="bg-white min-h-1/2  w-1/2 ">
+        {isLoading && <InfinitySpin width="200" color="#4fa94d" />}
+      </div>
+      <div className="bg-white min-h-1/2 max-h-1/2  w-1/2 ">
         <p>{recipe}</p>
       </div>
     </div>
